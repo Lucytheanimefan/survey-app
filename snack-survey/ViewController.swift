cdf7389efc093fb573054aee9ac930622e33f2c7
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-01-21 18:47:32 -0500

    Added conditions for data logging but buggy

--- snack-survey/ViewController.swift
+++ snack-survey/ViewController.swift
@@ -11,13 +11,18 @@ import ResearchKit
 
 class ViewController: UIViewController {
     var finishedConsent = false
-
+    var onConsent = false
+    var onSurvey = false
+    var i=0;
+    
+    var surveyResults:[String: [String]]=[:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-   
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -28,6 +33,8 @@ extension ViewController : ORKTaskViewControllerDelegate {
     
     
     func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        print("taskViewController CALLED "+String(i))
+        i += 1;
         let taskResult = taskViewController.result
         print("TASKRESULT--------")
         print(taskResult)
@@ -36,44 +43,64 @@ extension ViewController : ORKTaskViewControllerDelegate {
             print("STEP RESULT: ")
             print(stepResult)
             if ((stepResult.results?.count)!>0){
-                print("STEP RESULTS GREATER THAN 0")
-                print("SIGNATURE:")
-                let res = stepResult.results?[0] as? ORKConsentSignatureResult
-                let consent = res!.consented
-                if (consent){
-                    finishedConsent = consent;
+                //if (onConsent){
+                if (stepResult.identifier=="ConsentReviewStep"){
+                    let res = stepResult.results?[0] as? ORKConsentSignatureResult
+                    let consent = res!.consented
+                    if (consent){
+                        finishedConsent = consent;
+                    }
                 }
+                if (stepResult.identifier=="QuestionStep"){
+                    let choices = stepResult.results?[0] as! ORKTextQuestionResult
+                    let answers = choices.answer
+                    print("TEXT IDENTIFIER: "+stepResult.identifier)
+                    surveyResults[stepResult.identifier]=answers as! [String]?;
+                    
+                }else if (stepResult.identifier=="SnackStep" || stepResult.identifier=="IceCreamStep"){
+                    let choices = stepResult.results?[0] as! ORKChoiceQuestionResult
+                    let answers = choices.answer
+                    print("IDENTIFIER: "+stepResult.identifier)
+                    surveyResults[stepResult.identifier]=answers as! [String]?;
+                }
+                //}
             }
         }
         
+        print("SURVET RESULTS: ")
+        print(surveyResults)
         taskViewController.dismiss(animated: true, completion: nil)
     }
-
+    
     @IBAction func consentTapped(_ sender: Any) {
+        //onConsent = true
+        //onSurvey = false
         let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
         taskViewController.delegate = self
         present(taskViewController, animated: true, completion: nil)
     }
     
     @IBAction func surveyTapped(_ sender: Any) {
+        //onSurvey = true
+        //onConsent = false
         if (finishedConsent){
-        print("Survey TAPPED");
-        let taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
+            print("Survey TAPPED, new taskviewcontroller so we hope");
+            let taskViewController1 = ORKTaskViewController(task: SurveyTask, taskRun: nil)
+            taskViewController1.delegate = self
+            present(taskViewController1, animated: true, completion: nil)
         }else{
             let alert = UIAlertController(title: "Alert", message: "You did not sign the consent form. Please do that first.", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
     }
-
+    
     /*
-    @IBAction func consentTapped(sender : AnyObject) {
-        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
-        taskViewController.delegate = self
-        present(taskViewController, animated: true, completion: nil)
-    }*/
-
+     @IBAction func consentTapped(sender : AnyObject) {
+     let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+     taskViewController.delegate = self
+     present(taskViewController, animated: true, completion: nil)
+     }*/
+    
 }
 
